f7c16c189a56bd9751577a23107e3597e097a44d
Thibault Martin <twenty2bar+github@gmail.com>
2020-12-02 15:42:51 +0100

    Simplify

--- day2/day2.ml
+++ day2/day2.ml
@@ -1,44 +1,33 @@
-let rec read_line input f good wrong =
+let rec read_line input f good =
   try
     let line = input_line input in
     Scanf.sscanf line "%d-%d %c: %s" (fun lb ub c s ->
-        let good, wrong = f lb ub c s good wrong in
-        read_line input f good wrong
+        read_line input f (f lb ub c s good)
       )
   with
-  | End_of_file -> good, wrong
+  | End_of_file -> good
   | Scanf.Scan_failure _ | Failure _ ->
     Printf.printf "Wrong line format\n";
-    read_line input f good wrong
+    read_line input f good
 
 let read_data f filepath =
   let input = open_in filepath in
-  let good, wrong = read_line input f 0 0 in
+  let good = read_line input f 0 in
   close_in input;
-  good, wrong
+  good
 
-let part_one min max c s good wrong =
+let part_one min max c s good =
   let cpt = ref 0 in
   String.iter (fun c' -> if c = c' then incr cpt) s;
-  if !cpt < min || !cpt > max then
-    good, wrong+1
-  else
-    good+1, wrong
+  if !cpt >= min && !cpt <= max then good+1
+  else good
 
-let part_two fst lst c s good wrong =
-  let len = String.length s in
-  if len < lst then good, wrong+1
-  else if (s.[fst-1] = c) <> (s.[lst-1] = c) then
-    good+1, wrong
-  else
-    good, wrong+1
+let part_two fst lst c s good =
+  if String.length s < lst || (s.[fst-1] = c) = (s.[lst-1] = c) then good
+  else good+1
 
 let () =
-  let good, wrong = read_data part_one "data" in
-  Printf.printf "Part one :\n";
-  Printf.printf "%d wrong passwords\n" wrong;
-  Printf.printf "%d good passwords\n" good;
-  let good, wrong = read_data part_two "data" in
-  Printf.printf "Part two :\n";
-  Printf.printf "%d wrong passwords\n" wrong;
-  Printf.printf "%d good passwords\n" good;
+  let good = read_data part_one "data" in
+  Printf.printf "Part one : %d good passwords\n" good;
+  let good = read_data part_two "data" in
+  Printf.printf "Part two : %d good passwords\n" good

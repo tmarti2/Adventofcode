b4fe1bef37f6792e93a5c1c0a962464d09eb2ef5
Thibault Martin <twenty2bar+github@gmail.com>
2020-12-02 15:34:58 +0100

    :3

--- day2/day2.ml
+++ day2/day2.ml
@@ -1,47 +1,40 @@
+let rec read_line input f good wrong =
+  try
+    let line = input_line input in
+    Scanf.sscanf line "%d-%d %c: %s" (fun lb ub c s ->
+        let good, wrong = f lb ub c s good wrong in
+        read_line input f good wrong
+      )
+  with
+  | End_of_file -> good, wrong
+  | Scanf.Scan_failure _ | Failure _ ->
+    Printf.printf "Wrong line format\n";
+    read_line input f good wrong
+
 let read_data f filepath =
   let input = open_in filepath in
-  let good, wrong = f input 0 0 in
+  let good, wrong = read_line input f 0 0 in
   close_in input;
   good, wrong
 
-let rec part_one input good wrong =
-  try
-    let line = input_line input in
-    Scanf.sscanf line "%d-%d %c: %s" (fun min max c s ->
-        let cpt = ref 0 in
-        String.iter (fun c' -> if c = c' then incr cpt) s;
-        if !cpt < min || !cpt > max then
-          part_one input good (wrong+1)
-        else
-          part_one input (good+1) wrong
-      )
-  with
-  | End_of_file -> good, wrong
-  | Scanf.Scan_failure _ | Failure _ ->
-    Printf.printf "Wrong line format\n";
-    part_one input good (wrong+1)
+let part_one min max c s good wrong =
+  let cpt = ref 0 in
+  String.iter (fun c' -> if c = c' then incr cpt) s;
+  if !cpt < min || !cpt > max then
+    good, wrong+1
+  else
+    good+1, wrong
 
-let rec part_two input good wrong =
-  try
-    let line = input_line input in
-    Scanf.sscanf line "%d-%d %c: %s" (fun fst lst c s ->
-        let len = String.length s in
-        if len < lst then part_two input good (wrong+1)
-        else begin
-          if (s.[fst-1] = c) <> (s.[lst-1] = c) then
-            part_two input (good+1) wrong
-          else
-            part_two input good (wrong+1)
-        end
-      )
-  with
-  | End_of_file -> good, wrong
-  | Scanf.Scan_failure _ | Failure _ ->
-    Printf.printf "Wrong line format\n";
-    part_two input good (wrong+1)
+let part_two fst lst c s good wrong =
+  let len = String.length s in
+  if len < lst then good, wrong+1
+  else if (s.[fst-1] = c) <> (s.[lst-1] = c) then
+    good+1, wrong
+  else
+    good, wrong+1
 
 let () =
-  let good, wrong = read_data part_one "data"in
+  let good, wrong = read_data part_one "data" in
   Printf.printf "Part one :\n";
   Printf.printf "%d wrong passwords\n" wrong;
   Printf.printf "%d good passwords\n" good;

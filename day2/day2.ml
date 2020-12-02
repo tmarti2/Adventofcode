139339e829fa311ad6c783f9f62786d00d947867
Thibault Martin <twenty2bar+github@gmail.com>
2020-12-02 15:21:07 +0100

    use sscanf

--- day2/day2.ml
+++ day2/day2.ml
@@ -1,6 +1,3 @@
-let separators = Str.regexp "[- :]+"
-exception Too_many
-
 let read_data f filepath =
   let input = open_in filepath in
   let good, wrong = f input 0 0 in
@@ -10,54 +7,38 @@ let read_data f filepath =
 let rec part_one input good wrong =
   try
     let line = input_line input in
-    let str_list = Str.split separators line in
-    match str_list with
-    | min::max::letter::pwd::[] ->
-      let min = int_of_string min in
-      let max = int_of_string max in
-      let cpt = ref 0 in
-      String.iter (fun c ->
-          let str_c = Char.escaped c in
-          if String.equal letter str_c then begin
-            incr cpt;
-            if !cpt > max then raise Too_many
-          end
-        ) pwd;
-      if !cpt >= min then
-        part_one input (good+1) wrong
-      else
-        part_one input good (wrong+1)
-    | _ ->
-      Printf.printf "Wrong line format: %s\n" line;
-      part_one input good (wrong+1)
+    Scanf.sscanf line "%d-%d %c: %s" (fun min max c s ->
+        let cpt = ref 0 in
+        String.iter (fun c' -> if c = c' then incr cpt) s;
+        if !cpt < min || !cpt > max then
+          part_one input good (wrong+1)
+        else
+          part_one input (good+1) wrong
+      )
   with
   | End_of_file -> good, wrong
-  | Too_many -> part_one input good (wrong+1)
-
+  | Scanf.Scan_failure _ | Failure _ ->
+    Printf.printf "Wrong line format\n";
+    part_one input good (wrong+1)
 
 let rec part_two input good wrong =
   try
     let line = input_line input in
-    let str_list = Str.split separators line in
-    match str_list with
-    | min::max::letter::pwd::[] ->
-      let fst = int_of_string min in
-      let lst = int_of_string max in
-      let len = String.length pwd in
-      if len < lst then part_two input good (wrong+1)
-      else begin
-        let fst_c = Char.escaped pwd.[fst-1] in
-        let lst_c = Char.escaped pwd.[lst-1] in
-        if (fst_c = letter) <> (lst_c = letter) then
-          part_two input (good+1) wrong
-        else
-          part_two input good (wrong+1)
-      end
-    | _ ->
-      Printf.printf "Wrong line format\n";
-      part_two input good (wrong+1)
+    Scanf.sscanf line "%d-%d %c: %s" (fun fst lst c s ->
+        let len = String.length s in
+        if len < lst then part_two input good (wrong+1)
+        else begin
+          if (s.[fst-1] = c) <> (s.[lst-1] = c) then
+            part_two input (good+1) wrong
+          else
+            part_two input good (wrong+1)
+        end
+      )
   with
-   End_of_file -> good, wrong
+  | End_of_file -> good, wrong
+  | Scanf.Scan_failure _ | Failure _ ->
+    Printf.printf "Wrong line format\n";
+    part_two input good (wrong+1)
 
 let () =
   let good, wrong = read_data part_one "data"in
